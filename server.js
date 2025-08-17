e5d1a67c18bce8ac23d401647bfd9a8cda035513
aglucazgeorgeann-ship-it <aglucazgeorgeann@gmail.com>
2025-08-17 10:30:02 +0800

    Update server.js

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.get('/', (req, res) => {
 });
 
 // ================= START SERVER =================
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`✅ Server running on http://localhost:${PORT}`);
 });
